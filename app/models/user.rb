3e527ed950e1850a718188ac435b1eb4f16b1f80
hiroki-n-8 <nonomurahiroki0807@gmail.com>
2020-03-01 18:40:49 +0900

    add_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, presence: true
+  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
 end
