b2af18396b7ee1577b67dc4f70929769977c7928
hiroki-n-8 <nonomurahiroki0807@gmail.com>
2020-03-01 17:56:38 +0900

    add_column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name_kana, :last_name_kana, :first_name, :last_name, :birthday, :phone_number])
+    # devise_parameter_sanitizer.permit(:account_update, key: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, :phone_number, :introduction])
+  end
 end
