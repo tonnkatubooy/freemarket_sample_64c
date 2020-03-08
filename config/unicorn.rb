23f8d7e44ec1aa47cc33f272a33be79471e93b7c
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-03-08 13:57:32 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 app_path = File.expand_path('../../../', __FILE__)
-
+shared_path = File.expand_path('../../../shared/', __FILE__)
 worker_processes 1
 # currentを指定
 working_directory "#{app_path}/current"
