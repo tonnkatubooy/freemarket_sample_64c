c5e52dff61a4da5c9146c3977ff437fcab4ddc2b
hiroki-n-8 <nonomurahiroki0807@gmail.com>
2020-03-09 18:29:11 +0900

    create_creditcard-form

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,9 @@
 class CardsController < ApplicationController
+
   def index
   end
+
+  def new
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # devise_for :users
   root to: "users#index"
   resources :users, only: [:index]
-  resources :cards, only: [:index]
+  resources :cards, only: [:index, :new]
 end
