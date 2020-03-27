c4b0945e04441e3658e6555213479266eb2d2d57
satoru103 <58040354+satoru103@users.noreply.github.com>
2020-03-28 01:54:28 +0900

    1_items_purchase
    ・items#new,#createアクション設定
    ・ルーティング #new,create設定
    ・itemsモデルのアソシエーション
    ・itemsモデルのバリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,28 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+      if @item.save
+        redirect_to root_path
+      else
+        redirect_to new
+      end
+  end
+
   def show
   end
   
   def purchase
   end
 
+  private
+  def item_params
+    params.require(:item).permit(:item_name,:price,:description,:user,:category,:brand,:status,:delevery_charge,:area,:shipping_date,:shipping_method)
+  end
   
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   mount_uploader :image, ImageUploader
+
+  #Association
+  belongs_to :user
+  belongs_to :brand
+  belongs_to :category
+  has_many :comments
+  has_many :pictures
+
+  #validation
+  validates :item_name,:price,:description,:category,:status,:delevery_charge,:area,:shipping_date,:shipping_method
 end
