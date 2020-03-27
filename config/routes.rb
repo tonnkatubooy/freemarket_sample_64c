63b3938123a28a73633a423cba660f9cd4dc16a5
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-03-27 17:56:18 +0900

    space d

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items, only: [:index, :show, :new]
-  resources :items, only: [:index, :show]
   resources :items, only: [:index, :show] do
     member do
       get 'purchase'
      end
   end 
   resources :users, only: [:show]
-  master
 end
