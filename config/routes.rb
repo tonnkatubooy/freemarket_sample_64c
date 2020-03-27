967cac0ce5b34d8e218e9b5056a7dd5d4dcd2df8
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-03-27 17:46:17 +0900

    routes d

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
- create_exhibitpage
   resources :items, only: [:index, :show, :new]
   resources :items, only: [:index, :show]
   resources :items, only: [:index, :show] do
