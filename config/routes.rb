f1013f4cef2e29b1a9f353ac71bc1a2e25ae0bc0
hiroki-n-8 <nonomurahiroki0807@gmail.com>
2020-03-28 16:16:59 +0900

    change_routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items, only: [:index, :show]
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :addresses, only: [:new, :create, :edit, :update]
+  end
+  resources :cards, only: [:index, :new]
 end
