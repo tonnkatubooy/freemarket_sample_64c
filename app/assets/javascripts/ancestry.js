71d307a05d0b8da998a6d9d53a8d9f4f406fa133
halki <s.halki0203@icloud.com>
2020-04-04 18:07:38 +0900

    edit_ancestry6
    トップページでカテゴリーを表示する機能の実装

--- app/assets/javascripts/ancestry.js
+++ app/assets/javascripts/ancestry.js
@@ -1,13 +1,17 @@
+// $(function(){
+//   $("category_parents").hover(function(){
+//     var parentValue = $("category_parents").value;
+//     $.ajax({
+//       url: '/items/search',
+//       type: "GET",
+//       data: {
+//         parent_id: parentValue // 親ボックスの値をparent_idという変数にする。
+//       },
+//       dataType: 'json'
+//     });
+//   });
+// });
+
 $(function(){
-  $("category_parents").hover(function(){
-    var parentValue = $("category_parents").value;
-    $.ajax({
-      url: '/items/search',
-      type: "GET",
-      data: {
-        parent_id: parentValue // 親ボックスの値をparent_idという変数にする。
-      },
-      dataType: 'json'
-    });
-  });
+  $('category_parents').text('test');
 });
