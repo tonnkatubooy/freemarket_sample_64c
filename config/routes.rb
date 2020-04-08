a502231bdc92411ed09bc55e600cbc9cb91fa9a7
halki <s.halki0203@icloud.com>
2020-04-08 12:36:10 +0900

    edit_ancestry7
    ◎items_controllerに新しいアクションの追加
    ・newアクション
    ・get_category_childrenアクション
    ・get_category_grandchildrenアクション
    
    ◎items/viewディレクトリーに新ファイル作成
    ・get_category_children.json.jbuilder
    ・get_category_grandchildren.json.jbuilder
    ・new.html.haml
    
    ◎jsディレクトリーに新jsファイル作成
    ・category.js
    
    ◎routes.rbにAjaxで動くルーティングの追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:index, :show] do
+  resources :items, only: [:index, :show, :new] do
+  #Ajaxで動くアクションのルート
+  collection do
+    get 'get_category_children', defaults: { format: 'json' }
+    get 'get_category_grandchildren', defaults: { format: 'json' }
+  end
+end
+
 end
