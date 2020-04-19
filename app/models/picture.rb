f3a69d43f9281b378bcc42ea4d76fffbdb1f46f2
satoru103 <58040354+satoru103@users.noreply.github.com>
2020-04-20 00:23:51 +0900

    1_商品出品
    ・validationエラー表示のview作成
    ・商品出品画面のview修正
    ・addressフォームをactive_hash適用

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,7 @@
 class Picture < ApplicationRecord
-  mount_uploaders :image, ImageUploader
+  mount_uploader :image, ImageUploader
 
+  validates :image, presence: true
   #Association
   belongs_to :item
 end
