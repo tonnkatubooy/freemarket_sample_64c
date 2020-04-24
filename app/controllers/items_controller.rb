406452d6f9953b02081bc7747631f055643f8715
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-04-24 10:31:51 +0900

    add_action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path,notice:'商品を削除しました'
   end
 
   private
