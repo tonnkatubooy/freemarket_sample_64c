ac305feba199f2ba0251475e5c3af814f8e13fbf
satoru103 <58040354+satoru103@users.noreply.github.com>
2020-04-26 13:31:13 +0900

    1_product_purchase
    ・cardコントローラ、ルーティング
    ・pay.jpのインストール
    ・pay.jpのjsを読み込めるように設定を追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,12 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     resources :addresses, only: [:new, :create, :edit, :update]
   end
-  resources :cards, only: [:index, :new]
+
+  resources :card, only: [:index,:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 end
