652c365eef5782f51d6caa40bc2fb6e894d0b637
satoru103 <58040354+satoru103@users.noreply.github.com>
2020-05-03 16:39:47 +0900

    商品購入一部記載修正

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,28 @@
 class CardsController < ApplicationController
 
-  def index
+  require "payjp"
+
+  def new
+    card = Card.where(user_id: current_user.id)
+    redirect_to card_path(card) if card.exists?
+  end
+
+  def pay
+    Payjp.api_key = "sk_test_1547c1078a795141e2ee8623"
+    if params['payjp-token'].blank?
+      redirect_to new_card_path
+    else
+      customer = Payjp::Customer.create(
+      card: params['payjp-token'],
+      metadata: {user_id: current_user.id}
+      ) 
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to action: 'show'
+      else
+        redirect_to pay_cards_path
+      end
+    end
   end
 
   def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @address = Address.select(:user_id)
+    card = Card.where(user_id: current_user.id)
   end
 
 end
