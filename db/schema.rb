ec2dca82bfb594431854691b95a3f1936c31e943
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-05-07 14:43:41 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_05_04_130143) do
-
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "destination_first_name", null: false
     t.string "destination_last_name", null: false
