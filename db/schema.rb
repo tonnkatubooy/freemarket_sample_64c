a0202c12ea9c4fa56bc5c6ff054c6c106182c7db
halki <s.halki0203@icloud.com>
2020-05-07 19:31:44 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_05_04_130143) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
+
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "brand_name"
     t.datetime "created_at", null: false
@@ -104,6 +105,7 @@ ActiveRecord::Schema.define(version: 2020_05_04_130143) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
   add_foreign_key "cards", "users"
   add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
